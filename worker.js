7669efab30ad28c184251ea255aa518bb55aea9d
icheer <20741218+icheer@users.noreply.github.com>
2025-07-17 14:53:39 +0800

    fix: debug info

--- worker.js
+++ worker.js
@@ -31,7 +31,7 @@ async function handleRequest(request, env = {}) {
   if (apiPath === '/whoami') {
     return new Response(JSON.stringify({
       serverType: SERVER_TYPE,
-      serverInfo: isDeno ? Object.fromEntries(Deno.build) : request.cf || 'unknown',
+      serverInfo: isDeno ? Deno.build.toString() : request.cf || 'unknown',
       url: request.url,
       headers: Object.fromEntries(request.headers.entries()),
       method: request.method,
