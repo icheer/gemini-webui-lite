b0758ce54de7a44f9ed72d8d39bb16f6e6bcc9b4
icheer <20741218+icheer@users.noreply.github.com>
2025-10-15 16:47:39 +0800

    fix: ui

--- worker.js
+++ worker.js
@@ -1516,7 +1516,7 @@ function getHtmlContent() {
                     href="javascript:void(0)"
                     @click="previewImage(img)"
                   >
-                    🖼️图片{{ index + 1 }}
+                    📎 图片{{ index + 1 }}
                   </a>
                 </div>
               </div>
@@ -1601,7 +1601,7 @@ function getHtmlContent() {
                     href="javascript:void(0)"
                     @click="previewImage(img)"
                   >
-                    🖼️图片{{ index + 1 }}
+                    📎 图片{{ index + 1 }}
                   </a>
                 </div>
               </div>
